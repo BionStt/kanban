ccfb00e6225f50d6d3202bd407a6b27208dcc9f6
Emiliano Montesdeoca <emontesdeoc@gmail.com>
2018-05-29 23:10:11 +0100

    Update files, new features

--- js/main.js
+++ js/main.js
@@ -5,6 +5,8 @@ let arr = {
     done: []
 }
 
+let jsonbin = false;
+
 let todoList = document.getElementById('todoUl');
 let doingList = document.getElementById('doingUl');
 let doneList = document.getElementById('doneUl');
@@ -13,36 +15,44 @@ let input = document.getElementById("input");
 let secret = "";
 let bin = "";
 
-/// Drag and drop
-function allowDrop(ev) {
-    ev.preventDefault();
-}
-
-function drag(ev) {
-    ev.dataTransfer.setData("text", ev.target.id);
-}
-
-function drop(ev) {
-    ev.preventDefault();
-    let data = ev.dataTransfer.getData("text");
-    ev.target.children[2].appendChild(document.getElementById(data));
-
-    setCounter();
-    save();
-}
-
 /// press enter on input to add
 input.addEventListener("keyup", function (event) {
     event.preventDefault();
-    if (event.keyCode === 13)
+
+    let value = document.getElementById("input").value;
+    if (event.keyCode === 13) {
+        if (value.indexOf("add secret: ") != -1) {
+            secret = value.replace("add secret: ", "");
+            saveData("secret", secret);
+            return false;
+        }
+
+        if (value.indexOf("add bin: ") != -1) {
+            bin = value.replace("add bin: ", "");
+            saveData("bin", bin);
+            return false;
+        }
+
+        if (value.indexOf("delete settings") != -1) {
+            localStorage.removeItem("secret");
+            localStorage.removeItem("bin");
+            window.location.reload();
+        }
+
         add();
 
-    write();
-    save();
+        // write();
+        save();
+    }
+
+
 });
 
 /// Add card to todo
 function add() {
+
+    input = document.getElementById("input");
+
     if (input.value == "") {
         return false;
     }
@@ -50,7 +60,7 @@ function add() {
     let obj = {
         text: input.value,
         category: 'todo',
-        id: Math.random().toString(36).substring(7)
+        id: getRandomId()
     }
 
     input.value = "";
@@ -59,152 +69,92 @@ function add() {
     write(arr.todo, todoList)
 }
 
-/// write all cards
-function write(category, list) {
-    clear(list);
-
-    category.forEach(e => {
-        list.appendChild(getCard(e));
-    });
-
-    setCounter();
-}
-
-/// get card element
-function getCard(obj) {
-    let li = document.createElement('li');
-    li.innerHTML = obj.text + "<span class='btn-delete' removeid='" + obj.id + "' onclick='remove(event)'><i class='fas fa-times'></i></span>";
-    li.setAttribute("draggable", true);
-    li.setAttribute("ondragstart", "drag(event)");
-    li.id = obj.id;
-    return li;
-}
-
-/// clear ul
-function clear(element) {
-    let myNode = element;
-    while (myNode.firstChild) {
-        myNode.removeChild(myNode.firstChild);
-    }
-}
-
-/// get lis and set counters
-function setCounter() {
-    document.getElementById('todoCounter').innerHTML = document.getElementById('todoUl').children.length;
-    document.getElementById('doingCounter').innerHTML = document.getElementById('doingUl').children.length;
-    document.getElementById('doneCounter').innerHTML = document.getElementById('doneUl').children.length;
-}
-
-
-
 /// load
 function load() {
 
-    $.ajax({
-        url: 'https://api.jsonbin.io/b/' + bin + '/latest',
-        type: 'GET',
-        headers: { //Required only if you are trying to access a private bin
-            'secret-key': secret
-        },
-        success: (data) => {
-            arr.todo = data.todo;
-            arr.doing = data.doing;
-            arr.done = data.done;
+    if (!jsonbin) {
+        arr = JSON.parse(localStorage.getItem("data"));
 
-            write(arr.todo, todoList);
-            write(arr.doing, doingList);
-            write(arr.done, doneList);
+        write(arr.todo, todoList);
+        write(arr.doing, doingList);
+        write(arr.done, doneList);
 
-            setCounter();
-        },
-        error: (err) => {
-            console.log(err.responseJSON);
-        }
-    });
+
+    } else {
+
+        $.ajax({
+            url: 'https://api.jsonbin.io/b/' + bin + '/latest',
+            type: 'GET',
+            headers: {
+                'secret-key': secret
+            },
+            success: (data) => {
+                write(data.todo, todoList);
+                write(data.doing, doingList);
+                write(data.done, doneList);
+
+                setCounter();
+            },
+            error: (err) => {
+                console.log(err.responseJSON);
+            }
+        });
+
+    }
 }
-
-function remove(e) {
-    let parentElement = document.getElementById(e.srcElement.getAttribute("removeid"));
-    $("#" + parentElement.id).remove()
-    save();
-}
-
 
 function save() {
-    let newTodoList = [],
-        newDoingList = [],
-        newDoneList = [];
 
-    let curentTodoList = document.querySelectorAll('#todoUl > li');
-    let currentDoingList = document.querySelectorAll('#doingUl > li');
-    let currentDoneList = document.querySelectorAll('#doneUl > li');
-
-    curentTodoList.forEach(element => {
-        let obj = {
-            text: element.innerText,
-            category: 'todo',
-            id: element.id
-        }
-        newTodoList.push(obj);
-    });
-
-    currentDoingList.forEach(element => {
-        let obj = {
-            text: element.innerText,
-            category: 'doing',
-            id: element.id
-        }
-        newDoingList.push(obj);
-    });
-
-    currentDoneList.forEach(element => {
-        let obj = {
-            text: element.innerText,
-            category: 'done',
-            id: element.id
-        }
-        newDoneList.push(obj);
-    });
+    let newTodoList = getTicketsByCategory("todo", "#todoUl > li");
+    let newDoingList = getTicketsByCategory("doing", "#doingUl > li");
+    let newDoneList = getTicketsByCategory("done", "#doneUl > li");
 
     arr.todo = newTodoList;
     arr.doing = newDoingList;
     arr.done = newDoneList;
 
-    $.ajax({
-        url: 'https://api.jsonbin.io/b/' + bin,
-        type: 'PUT',
-        headers: { //Required only if you are trying to access a private bin
-            'secret-key': secret
-        },
-        contentType: 'application/json',
-        data: JSON.stringify(arr),
-        success: (data) => {
-            console.log(data);
-        },
-        error: (err) => {
-            console.log(err.responseJSON);
-        }
-    });
+    if (!jsonbin) {
+
+        localStorage.setItem("data", JSON.stringify(arr));
+
+
+    } else {
+
+
+        $.ajax({
+            url: 'https://api.jsonbin.io/b/' + bin,
+            type: 'PUT',
+            headers: {
+                'secret-key': secret
+            },
+            contentType: 'application/json',
+            data: JSON.stringify(arr),
+            success: (data) => {
+                // console.log(data);
+            },
+            error: (err) => {
+                console.log(err.responseJSON);
+            }
+        });
+
+    }
+
 }
 
 function getLocalstorage() {
-    var info = JSON.parse(localStorage.getItem("info"));
+    var secretStored = JSON.parse(localStorage.getItem("secret"));
+    var binStored = JSON.parse(localStorage.getItem("bin"));
 
-    if (info == null) {
-        input.setAttribute("disabled", true);
-        input.value = "Set your settings first!"
+    if (secretStored == null || binStored == null || secretStored == "" || binStored == "") {
+        jsonbin = false;
         return false;
     }
 
-    if (info.secret == null || info.secret == "" || info.bin == null || info.bin == "") {
-        input.setAttribute("disabled", true);
-        input.value = "Settings are wrong!"
+    jsonbin = true;
 
-        return false;
-    }
+    secret = secretStored;
+    bin = binStored;
 
-    secret = info.secret;
-    bin = info.bin;
     var dark = JSON.parse(localStorage.getItem("dark"));
 
     if (dark == null) {
@@ -213,65 +163,41 @@ function getLocalstorage() {
 
     if (dark) {
         document.getElementById("container").className = "container dark";
-        document.getElementById("settings").className = "button btn-round button-dark";
         document.getElementById("dark").className = "button btn-round button-dark";
-        document.getElementById("savebtn").className = "button btn-round button-save-dark";
-        document.getElementById("modal").className = "modal modal-dark";
     }
-
-    document.getElementById("secret").value = secret;
-    document.getElementById("bin").value = bin;
-    document.getElementById("dark").checked = dark;
 
     return true;
 
 }
 
 function darkmode() {
-
     var dark = false
+
     if (document.getElementById("container").className == "container") {
         dark = true
     }
 
     if (dark) {
         document.getElementById("container").className = "container dark";
-        document.getElementById("settings").className = "button btn-round button-dark";
         document.getElementById("dark").className = "button btn-round button-dark";
-        document.getElementById("savebtn").className = "button btn-round button-save-dark";
-        document.getElementById("modal").className = "modal modal-dark";
     } else {
 
         document.getElementById("container").className = "container";
-        document.getElementById("settings").className = "button btn-round ";
         document.getElementById("dark").className = "button btn-round ";
-        document.getElementById("savebtn").className = "button btn-round button-save-dark";
-        document.getElementById("modal").className = "modal ";
     }
 
     localStorage.setItem("dark", JSON.stringify(dark));
-
 }
 
-function saveData() {
+function saveData(key, value) {
+    var localstorage = JSON.parse(localStorage.getItem(key));
+    localStorage.setItem(key, JSON.stringify(value));
 
-    var secret = document.getElementById("secret").value;
-    var bin = document.getElementById("bin").value;
-
-    var obj = {
-        secret: secret,
-        bin: bin
-    }
-
-    localStorage.setItem("info", JSON.stringify(obj));
     window.location.reload(false);
 }
 
 $(function () {
 
-    if (getLocalstorage()) {
-        load();
-
-    }
-
+    getLocalstorage();
+    load();
 });
---
+++ js/utils.js
@@ -0,0 +1,85 @@
+/// Drag and drop
+function allowDrop(ev) {
+    ev.preventDefault();
+}
+
+function drag(ev) {
+    ev.dataTransfer.setData("text", ev.target.id);
+}
+
+function drop(ev) {
+    ev.preventDefault();
+    let data = ev.dataTransfer.getData("text");
+    ev.target.children[2].appendChild(document.getElementById(data));
+
+    setCounter();
+    save();
+}
+
+/// get lis and set counters
+function setCounter() {
+    document.getElementById('todoCounter').innerHTML = document.getElementById('todoUl').children.length;
+    document.getElementById('doingCounter').innerHTML = document.getElementById('doingUl').children.length;
+    document.getElementById('doneCounter').innerHTML = document.getElementById('doneUl').children.length;
+}
+
+/// clear ul
+function clear(element) {
+    try {
+        let myNode = element;
+        while (myNode.firstChild) {
+            myNode.removeChild(myNode.firstChild);
+        }
+    } catch (error) {}
+
+}
+
+/// get card element
+function getCard(obj) {
+    let li = document.createElement('li');
+    li.innerHTML = obj.text + "<span class='btn-delete'  removeid='" + obj.id + "' onclick='remove(event)'></span>";
+    li.setAttribute("draggable", true);
+    li.setAttribute("ondragstart", "drag(event)");
+    li.id = obj.id;
+    return li;
+}
+
+/// write all cards
+function write(category, list) {
+    clear(list);
+
+    category.forEach(e => {
+        list.appendChild(getCard(e));
+    });
+
+    setCounter();
+}
+
+/// remove using the button from the ticket
+function remove(e) {
+    let parentElement = document.getElementById(e.srcElement.getAttribute("removeid"));
+    $("#" + parentElement.id).remove()
+    save();
+}
+
+/// Get random id
+function getRandomId() {
+    return Math.random().toString(36).substring(7);
+}
+
+function getTicketsByCategory(categoryName, jqueryPath) {
+    let returnList = [];
+    let curentList = document.querySelectorAll(jqueryPath);
+
+    curentList.forEach(element => {
+        let obj = {
+            text: element.innerText,
+            category: categoryName,
+            id: element.id
+        }
+        returnList.push(obj);
+    });
+
+    return returnList;
+
+}
